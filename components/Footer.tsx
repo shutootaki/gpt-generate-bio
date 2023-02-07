53bce47251e377aa971fa7e60ce1d61da4a86906
shutootaki <ootaki200109@gmail.com>
2023-02-08 07:44:52 +0900

    create component

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        Powered by{" "}
+        <a href="https://openai.com/" target="_blank" rel="noreferrer">
+          Open AI
+        </a>
+      </div>
+    </footer>
+  );
+};

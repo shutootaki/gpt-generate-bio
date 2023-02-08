2ca437b0ff7951037d695d6a41a8c7d27ee98ba4
shutootaki <ootaki200109@gmail.com>
2023-02-08 21:28:41 +0900

    feat: header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import Link from "next/link";
+import { GiArtificialIntelligence } from "react-icons/gi";
+import { GitHub } from "./GitHub";
 
 export const Header = () => {
-  return <div>GPT Generate BIO</div>;
+  return (
+    <header className="flex justify-between items-center border-b-2 mt-6 pb-8 sm:px-4 px-2">
+      <Link href="/" className="flex space-x-3">
+        <GiArtificialIntelligence size={40} />
+        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
+          GPT Generate BIO
+        </h1>
+      </Link>
+      <GitHub />
+    </header>
+  );
 };

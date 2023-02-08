ca312b0af2bc056a2e88fcf28f2420fce1c4abd1
shutootaki <ootaki200109@gmail.com>
2023-02-08 21:29:35 +0900

    chore: github link

---
+++ components/GitHub.tsx
@@ -0,0 +1,13 @@
+import { FaGithubAlt } from "react-icons/fa";
+
+export const GitHub = () => {
+  return (
+    <a
+      className="flex max-w-5xl items-center justify-center space-x-2 rounded-full border-2 border-gray-300  px-4 py-2 text-md transition-colors hover:bg-slate-600"
+      href="https://github.com/shutootaki/gpt-generate-bio"
+    >
+      <p>View on GitHub</p>
+      <FaGithubAlt />
+    </a>
+  );
+};

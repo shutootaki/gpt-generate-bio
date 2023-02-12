aa0d158ea2b8fdef89ec4e2a48bdea6875ff70d6
shutootaki <ootaki200109@gmail.com>
2023-02-12 23:14:10 +0900

    feat: connect api

---
+++ pages/api/generate.ts
@@ -0,0 +1,28 @@
+import { OpenAIStream } from "../utils/OpenAIStream";
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing env var from OpenAI");
+}
+
+const handler = async (req: Request): Promise<Response> => {
+  const prompt = (await req.json()) as { prompt?: string };
+
+  if (prompt) {
+    return new Response("No prompt in the request", { status: 400 });
+  }
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};

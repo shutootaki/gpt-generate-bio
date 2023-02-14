5ea084bd2f8ed39c3c116ffd9af61dc93cd826e9
shutootaki <ootaki200109@gmail.com>
2023-02-14 22:42:32 +0900

    chore: link

--- components/GenerateButton.tsx
+++ components/GenerateButton.tsx
@@ -1,16 +1,28 @@
-export const GenerateButton = ({
-  generateBio,
-}: {
+import { Loading } from "./Loading";
+
+type Props = {
+  loading: boolean;
   generateBio: (e: React.MouseEvent) => Promise<void>;
-}) => {
+};
+export const GenerateButton = ({ generateBio, loading }: Props) => {
   return (
     <div className="max-w-xl w-full">
-      <button
-        className="bg-purple-700 rounded-xl text-slate-100 font-medium px-4 py-2 sm:mt-12 mt-8 hover:bg-purple-800 w-full"
-        onClick={(e) => generateBio(e)}
-      >
-        Generate your bio &rarr;
-      </button>
+      {!loading && (
+        <button
+          className="bg-purple-700 rounded-xl text-slate-100 font-medium px-4 py-2 sm:mt-12 mt-8 hover:bg-purple-800 w-full"
+          onClick={(e) => generateBio(e)}
+        >
+          Generate your bio &rarr;
+        </button>
+      )}
+      {loading && (
+        <button
+          className="bg-purple-700 rounded-xl text-slate-100 font-medium px-4 py-2 sm:mt-12 mt-8 hover:bg-purple-800 w-full"
+          onClick={(e) => generateBio(e)}
+        >
+          <Loading />
+        </button>
+      )}
     </div>
   );
 };
